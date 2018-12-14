2ad17a317015c02ce66328cc56207445a65895e0
dna <ping.divya@gmail.com>
2018-12-13 19:42:18 -0500

    Update cachematrix.R
    assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## creates a special matrix that can cache its inverse
+## input: invertable matrix 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-	print(paste0("hello world"))
-
+	xinv <- NULL
+	set <- function(y) {
+		x <<- y
+		xinv <<- NULL
+	}
+	get <- function() x
+	setinv <- function(inv) xinv <<- inv
+	getinv <- function() xinv
+	list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## calculates the inverse of a special matrix 'x'
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        xinv <- x$getinv()
+        if(!is.null(xinv)) {
+                message("getting cached data")
+                return(xinv)
+        }
+        data <- x$get()
+        xinv <- solve(data, ...)
+        x$setinv(xinv)
+        xinv
 }
